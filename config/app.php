7d9e218e0c09987b82554df7fb200080ddf4b671
Ajeet Kumar Tripathi <ajeet.triapthi632@gmail.com>
2016-12-03 12:05:03 +0900

    passport added

--- config/app.php
+++ config/app.php
@@ -162,6 +162,11 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        /**
+         * passport
+         */
+        Laravel\Passport\PassportServiceProvider::class,
+
         /*
          * debugger
          */
