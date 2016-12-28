1fc0395a3d4078f6b1f79b17d899d67ef3a3146c
Ajeet Kumar Tripathi <ajeet.triapthi632@gmail.com>
2016-12-28 12:43:33 +0900

    abc

---
+++ resources/views/access/users/component/register.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ajeetkumar
+ * Date: 2016/12/20
+ * Time: 23:54
+ */
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,9 +6,11 @@
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
                 <div class="panel-heading">Dashboard</div>
-                <passport-clients></passport-clients>
-                <passport-authorized-clients></passport-authorized-clients>
-                <passport-personal-access-tokens></passport-personal-access-tokens>
+                <div class="panel-body">
+                    <passport-clients></passport-clients>
+                    <passport-authorized-clients></passport-authorized-clients>
+                    <passport-personal-access-tokens></passport-personal-access-tokens>
+                </div>
             </div>
         </div>
     </div>
